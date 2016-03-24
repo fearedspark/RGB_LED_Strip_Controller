c7f2c761af42a20a6aad50ed44031f0b781806f9
Thomas Gonnot <thomas@gonnot.me>
2016-03-24 12:41:25 -0500

    First project commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RGB_LED_Strip_Controller
-Firmware for a RGB LED Strip Controller, to control NeoPixel or DotStar LED strips with a STM32F0 microcontroller. Hardware available on circuitmaker at http://circuitmaker.com/Projects/E018036A-EE0F-4A8F-B97C-0EB9E18DF42C
+## Description
+  Firmware for a RGB LED Strip Controller, to control NeoPixel or DotStar LED strips with a STM32F0 microcontroller. Hardware available on circuitmaker at http://circuitmaker.com/Projects/E018036A-EE0F-4A8F-B97C-0EB9E18DF42C
+## Known Issues
+  *None so far!
+## To do
+  *Implement the NeoPixel protocol
+  *Send update only when data changed
+  *Put the microcontroller to sleep and wake up only on interrupts
+## Hardware
+  http://circuitmaker.com/Projects/E018036A-EE0F-4A8F-B97C-0EB9E18DF42C
---
+++ Src/main.c
@@ -0,0 +1,355 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#include "math.h"
+#define NLEDS 30
+//Mode: NeoPixel = 0, DotStar = 2 != 0
+#define ModeInput() GPIOB->IDR & 0x00000002
+#define GetColorRotB() GPIOA->IDR & 0x00000002
+#define GetPwrRotB() GPIOA->IDR & 0x00000008
+#define HUE_INCREMENT 2
+#define INTENSITY_INCREMENT 0.05
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+DMA_HandleTypeDef hdma_spi1_tx;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint32_t colors_buffer[NLEDS+2];
+uint32_t * colors_array = colors_buffer + 1;
+double LedIntensity = 0;
+double LedHue = 0;
+uint32_t DisplayWhite = 1;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_SPI1_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+uint32_t RGB(uint8_t R, uint8_t G, uint8_t B);
+uint32_t Hue2RGB(double H, double V);
+void TransmitNeopixel(uint32_t * colors);
+void TransmitDotstar(uint32_t * colors);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+	int i;
+	double f;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_SPI1_Init();
+
+  /* USER CODE BEGIN 2 */
+	colors_buffer[0] = 0x00000000;
+	colors_buffer[NLEDS + 1] = 0xFFFFFFFF;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+		for(f = 0; f < 360.0; f += 0.1)
+		{
+			for(i = 0; i < NLEDS; i++)
+				if(DisplayWhite)
+					colors_array[i] = RGB(255.0*LedIntensity, 255.0*LedIntensity, 255.0*LedIntensity);
+				else
+					colors_array[i] = Hue2RGB(LedHue, LedIntensity);
+			TransmitDotstar(colors_buffer);
+		}
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* SPI1 init function */
+void MX_SPI1_Init(void)
+{
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_1LINE;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  HAL_SPI_Init(&hspi1);
+
+}
+
+/** 
+  * Enable DMA controller clock
+  */
+void MX_DMA_Init(void) 
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel2_3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel2_3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel2_3_IRQn);
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pins : ColorRotA_Pin ColorButton_Pin PwrRotA_Pin PwrButton_Pin */
+  GPIO_InitStruct.Pin = ColorRotA_Pin|ColorButton_Pin|PwrRotA_Pin|PwrButton_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ColorRotB_Pin PwrRotB_Pin */
+  GPIO_InitStruct.Pin = ColorRotB_Pin|PwrRotB_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : Mode_Pin */
+  GPIO_InitStruct.Pin = Mode_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(Mode_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI2_3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+uint32_t RGB(uint8_t R, uint8_t G, uint8_t B)
+{
+    return __REV(0xFF000000 | (uint32_t)B << 16 | (uint32_t)G << 8 | (uint32_t)R);
+}
+
+uint32_t Hue2RGB(double H, double V)
+{
+    H = fmod(H, 360);
+    if(V > 1)
+        V = 1;
+    if(V < 0)
+        V = 0;
+    double X = V * (1 - fabs(fmod(H / 60, 2) - 1));
+    int S = ((uint32_t)H) / 60;
+    switch(S)
+    {
+        case 0:
+            return RGB((uint8_t)(V*255), (uint8_t)(X*255), 0);
+        case 1:
+            return RGB((uint8_t)(X*255), (uint8_t)(V*255), 0);
+        case 2:
+            return RGB(0, (uint8_t)(V*255), (uint8_t)(X*255));
+        case 3:
+            return RGB(0, (uint8_t)(X*255), (uint8_t)(V*255));
+        case 4:
+            return RGB((uint8_t)(X*255), 0, (uint8_t)(V*255));
+        case 5:
+            return RGB((uint8_t)(V*255), 0, (uint8_t)(X*255));
+        default:
+            return RGB(0, 0, 0);
+    }
+}
+
+void TransmitNeopixel(uint32_t * colors)
+{
+}
+
+void TransmitDotstar(uint32_t * colors)
+{
+	HAL_StatusTypeDef spi_status;
+	do
+	{
+		spi_status = HAL_SPI_Transmit_DMA(&hspi1, (uint8_t *)colors, (NLEDS+2)*4);
+	}
+	while(spi_status == HAL_BUSY);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	switch(GPIO_Pin)
+	{
+		case ColorRotA_Pin:
+			DisplayWhite = 0;
+			if(GetColorRotB())
+				LedHue += HUE_INCREMENT;
+			else
+				LedHue -= HUE_INCREMENT;
+			LedHue = fmod(LedHue, 360);
+			break;
+		case ColorButton_Pin:
+			if(DisplayWhite)
+				DisplayWhite = 0;
+			else
+				DisplayWhite = 1;
+			break;
+		case PwrRotA_Pin:
+			if(GetPwrRotB())
+				LedIntensity += INTENSITY_INCREMENT;
+			else
+				LedIntensity -= INTENSITY_INCREMENT;
+			if(LedIntensity > 1)
+				LedIntensity = 1;
+			if(LedIntensity < 0)
+				LedIntensity = 0;
+			break;
+		case PwrButton_Pin:
+			if(LedIntensity > 0)
+				LedIntensity = 0;
+			else
+				LedIntensity = 1;
+			break;
+	}
+}
+
+/* USER CODE END 4 */
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
